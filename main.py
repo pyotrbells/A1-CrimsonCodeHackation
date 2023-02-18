f9ab96fe1a39e56e2846258c8aeb29455cc0f09a
kstaff03 <125761460+kstaff03@users.noreply.github.com>
2023-02-18 15:46:59 -0800

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,17 @@ from discord import app_commands
 from discord.ext import commands
 from typing import Optional
 
+bot = commands.Bot(command_prefic="!", intents = discord.Intents.all())
+
+@bot.event
+async def on_ready():
+    print("Bot is ready")
+    try:
+        sync = await bot.tree.sync()
+        print(f"Synced {len(synced)} command(s)")
+    except Exception as e:
+        print(e)
+
 @bot.tree.command(name="shop")
 async def shop(interaction: discord.Interaction, Display_pets:Optional[bool], Display_items:Optional[bool]):
     #display pets and items
