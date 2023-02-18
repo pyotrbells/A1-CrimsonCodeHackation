d688816758e8597f8e20292410a2151c92ed932a
skyhu2 <senlia04@gmail.com>
2023-02-18 15:08:23 -0800

    Update main.py

--- main.py
+++ main.py
@@ -1,2 +1,8 @@
 from typing import Optional
 async def shop(interaction: discord.Interaction, Display_pets:Optional[bool], Display_items:Optional[bool]):
+    #display pets and items
+
+    #use if-statements to determine what they choose to buy
+    #within if statement, add that item to the specific list and subtract the money from the wallet
+
+    #Ask if they would like to continue shopping
